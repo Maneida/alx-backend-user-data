52178dcc331ac8dc834286b43991135edce9ca43
Michael Amuah <mkdamuah@hotmail.com>
2023-11-16 23:51:40 +0000

    bd

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -36,3 +38,10 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Searches for a user in the database using the given attribute """
+        result = self._session.query(User).filter_by(**kwargs).all()
+        if not result:
+            raise NoResultFound
+        return result[0]
